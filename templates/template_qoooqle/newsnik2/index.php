a73bccd2f88b6b4368d03d333daf256505ffcd8d
ripper <ripper@users.noreply.github.com>
2026-07-09 13:08:59 +0300

    Asset: Add empty 0-byte placeholder files for newsnik favicons and link them in template index.php files

--- templates/template_qoooqle/newsnik2/index.php
+++ templates/template_qoooqle/newsnik2/index.php
@@ -99,7 +99,7 @@ $html_lang = ($lang === 'cz') ? 'cs' : $lang;
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;1,9..40,400&family=KoHo:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="icon" href="./favicon.svg">
+    <link rel="icon" href="./favicon.png">
     <style>
         /* ============================================================
            RESET & BASE
--- templates/template_qoooqle/newsnik3/index.php
+++ templates/template_qoooqle/newsnik3/index.php
@@ -85,7 +85,7 @@ $reg_url = $offer_register_url . (strpos($offer_register_url, '?') !== false ? '
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;1,9..40,400&display=swap" rel="stylesheet">
-    <link rel="icon" href="./favicon.svg">
+    <link rel="icon" href="./favicon.ico">
     <style>
         /* ============================================================
            RESET & BASE
