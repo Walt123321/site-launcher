efe72dc7d31706b41c4bb1385c293308421b9b67
ripper <ripper@users.noreply.github.com>
2026-08-18 13:22:49 +0300

    Revert "T6: restore secondary phone overlay image (revert b371c52's removal)"
    This reverts commit d7b74b781b2081cc54b86699b80f1597112af8ee.

--- templates/template_6/sv/index.php
+++ templates/template_6/sv/index.php
@@ -698,15 +698,6 @@ $canonical = 'https://' . $host . $uri;
                         height="640"
                         loading="lazy"
                         decoding="async">
-
-                    <img
-                        src="../assets/img/mobile-trading-light.webp"
-                        alt="<?= $mobile_trading_visual_alt ?>"
-                        class="app-secondary-image"
-                        width="1086"
-                        height="1448"
-                        loading="lazy"
-                        decoding="async">
                 </div>
     
                 <div>
